7e949313c82e8c9eddbb9df586a4f613fe7962dc
Jostin <jostindsouza30@gmail.com>
2023-05-23 15:06:05 +0530

    Initial Commit

---
+++ Flag Play/Flag_PlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_PlayApp.swift
+//  Flag Play
+//
+//  Created by Jostin on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Flag_PlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
